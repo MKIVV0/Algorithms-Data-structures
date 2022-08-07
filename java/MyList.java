99bf773e2079ad092e600a31405c8fc63171caf8
MKIVV0 <74113303+MKIVV0@users.noreply.github.com>
2022-08-07 14:19:40 +0200

    Add files via upload

---
+++ java/MyList.java
@@ -0,0 +1,110 @@
+/*
+* @author Matteo Huang
+* My implementation of the concatenated list data structure
+*/
+
+public class MyList<Item> {
+    /*
+    * represents the head of the list
+    */
+    private Node first;
+
+    /*
+    * represents the list's length
+    */
+    private int N;
+
+    /*
+    * the class Node represents a single node, which has
+    * as fields an item of a generic type Item and a reference
+    * of type Node to the next node
+    */
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    /*
+    * the method isEmpty() checks whether the list is empty
+    * Time complexity: O(1) = Θ(1) = Ω(1)
+    * @param l list
+    */
+    public boolean isEmpty(MyList l){
+        return l == null;
+    }
+
+    /*
+    * the method length() returns the list's length
+    * Time Complexity: O(1) = Θ(1) = Ω(1)
+    * @param void
+    */
+    public int length(){
+        return N;
+    }
+
+    /*
+     * the method insert() inserts an element at position i
+     * Time Complexity: O(1); Θ(n); Ω(n)
+     * @param item element to insert
+     * @param i insertion index
+     */
+    public void insert(Item item, int i){
+        Node prev = first;
+        Node node = new Node();
+        node.item = item;
+        if (i == 0) {
+            first = node;
+            first.next = null;
+        }
+        else {
+            for (int j = 0; i < j; j++) {
+                prev = prev.next;
+            }
+            if(prev == null) {
+                prev = node;
+                prev.next = null;
+            } else {
+                node.next = prev.next;
+                prev.next = node;
+            }
+        }
+        N++;
+    }
+
+    /*
+     * the method delete() deletes an element at position i
+     * Time Complexity: O(1); Θ(n); Ω(n)
+     * @param i deletion index
+     */
+    public void delete(int i){
+        Node prev = first;
+        Node curr = first;
+        if (N == 0) return;
+        else {
+            for (int j = 0; j < i; j++) {
+                curr = prev;
+                prev = prev.next;
+            }
+            if (prev.next == null)
+                prev = null;
+            else {
+                curr.next = prev.next;
+            }
+        }
+        N--;
+    }
+
+    /*
+     * the method printList() prints the list
+     * Time Complexity: O(1); Θ(n); Ω(n)
+     * @param void
+     */
+    public void printList(){
+        Node node = first;
+        int i = 0;
+        while(node != null) {
+            System.out.println(i++ + " " + node.item);
+            node = node.next;
+        }
+    }
+}
