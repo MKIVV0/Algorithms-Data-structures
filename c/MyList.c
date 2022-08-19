117150fb9f94b31b5e6449a839324331fd939274
Matt <huangzhangying@live.it>
2022-08-20 00:38:35 +0200

    fixed the delete() method for index > 0 still have to fix the case where the index = 0

--- c/MyList.c
+++ c/MyList.c
@@ -25,19 +25,18 @@ int insert(NODE* list, int idx, ITEM item) {
 	
 	int i = 0;
 
+	NODE* tmp = (NODE*)malloc(sizeof(NODE));
 	if (idx == 0) {
-		NODE* another = (NODE*)malloc(sizeof(NODE));
-		another->item = list->item;
-		another->next = list->next;
+		tmp->item = list->item;
+		tmp->next = list->next;
 		list->item = item;
-		list->next = another;
+		list->next = tmp;
 	} else {
 		while ((i < idx - 1) && (list != NULL)) {
 			i++;
 			list = list->next;
 		}
 
-		NODE* tmp = (NODE*)malloc(sizeof(NODE));
 		tmp->next = list->next;
 		tmp->item = item;
 		list->next = tmp;
@@ -49,6 +48,21 @@ int insert(NODE* list, int idx, ITEM item) {
 int delete(NODE* list, int idx) {
 	if (list == NULL) return -1;
 
+	if (idx == 0) {
+		NODE* tmp = list;
+		list = list->next;
+		free(tmp);
+	} else {
+		int i = 0;
+		NODE* curr = list;
+		while ((i < idx) && list != NULL) {
+			i++;
+			list = curr;
+			curr = curr->next;
+		}
+		list->next = curr->next;
+		free(curr);
+	}
 
 	return 0;
 }
@@ -72,9 +86,22 @@ int main() {
 	NODE* list = initList(0);
 	insert(list, 1, 1);
 	insert(list, 2, 2);
-    insert(list, 2, 10);
+    insert(list, 3, 10);
+	insert(list, 0, -1);
 	printList(list);
-	delete(list, 1);
+	delete(list, 0);
+	printf("\nPRINT POST DELETE\n");
 	printList(list);
+	//printf("\n%d\n", list->next->next->item);
+	
+	/* FUNZIONANTE (LA STESSA COSA LA FA IL METODO delete(), MA NON VA NIENTE)
+	NODE* next = list;
+	NODE* curr = list->next;
+	free(next);
+	printf("%d ", curr->item);
+	printf("%d ", curr->next->item);
+	printf("%d ", curr->next->next->item);
+	printf("%d ", curr->next->next->next->item);
+	*/
 	return 0;
 }
